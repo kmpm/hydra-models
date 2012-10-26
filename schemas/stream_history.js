a4ac030f46c1401c0d531c6cbe50aaac4f579304
Peter Magnusson <peter@birchroad.net>
2012-10-26 10:51:31 +0200

    added a virtual .date method to StreamHistory
    formats timestamp using moment.js

--- schemas/stream_history.js
+++ schemas/stream_history.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose')
-  , Schema = mongoose.Schema;
-
+  , Schema = mongoose.Schema
+  , moment = require('moment');
 
 var streamHistorySchema = new Schema({
   stream: {type:Schema.Types.ObjectId, ref:'Stream', required: true},
@@ -13,6 +13,9 @@ var streamHistorySchema = new Schema({
 
 streamHistorySchema.index({stream: 1, timestamp:1});
 
+streamHistorySchema.virtual('date').get(function () {
+  return moment(this.timestamp).format('YYYY-MM-DD hh:mm:ss');
+});
 
 
 module.exports = streamHistorySchema;
